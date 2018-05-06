5dc06e73406d2b623cd3c0ff4f783d598d42faeb
Hsieh Yuho <yuho.yhhsieh@gmail.com>
2018-05-06 17:52:48 +0800

    remove mini

--- Full_Stack/Mini-Google-Search-Engine/Indexer/IndexerHW3/src/main/java/edu/upenn/cis/stormlite/spout/WordFileSpout.java
+++
@@ -1,8 +0,0 @@
-package edu.upenn.cis.stormlite.spout;
-
-public class WordFileSpout extends FileSpout {
-    @Override
-	public String getFilename() {
-		return "words.txt";
-	}
-}
--- Full_Stack/Mini-Google-Search-Engine/Indexer/IndexerHW3/src/main/java/edu/upenn/cis455/mapreduce/Context.java
+++
@@ -1,7 +0,0 @@
-package edu.upenn.cis455.mapreduce;
-
-public interface Context {
-
-  void write(String key, String value);
-  
-}
